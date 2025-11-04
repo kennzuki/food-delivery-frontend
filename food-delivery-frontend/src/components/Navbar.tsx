b1c00e7bc1b142b31d10bb559b81ce19cf19a249
kennzuki <kingsyken@gmail.com>
2025-11-04 15:31:36 +0300

    navbar list items set to active links

--- food-delivery-frontend/src/components/Navbar.tsx
+++ food-delivery-frontend/src/components/Navbar.tsx
@@ -1,17 +1,39 @@
+import { useState } from 'react';
 import { FaShoppingCart } from 'react-icons/fa';
 const Navbar = () => {
+  const [menu, setMenu] = useState('menu');
   return (
-    <div className='max-w-6xl mx-auto p-8 flex justify-evenly'>
-      <h1 className='text-3xl font-bold underline uppercase text-green-500'>
+    <div className='max-w-6xl mx-auto p-8 flex justify-evenly cursor-pointer'>
+      <h1 className='text-3xl font-bold underline uppercase text-green-500 '>
         {' '}
         Kenki deli{' '}
       </h1>
-      <nav className='flex gap-2 place-items-center'>
-        <a href='/'>Home</a>
-        <a href='/menu'>Menu</a>
-        <a href='/about'>About</a>
-        <a href='/contact'>Contact-us</a>
-      </nav>
+      <ul className='flex gap-2 place-items-center capitalize'>
+        <li
+          onClick={() => setMenu('home')}
+          className={menu === 'home' ? 'underline' : ''}
+        >
+          home
+        </li>
+        <li
+          onClick={() => setMenu('menu')}
+          className={menu === 'menu' ? 'underline' : ''}
+        >
+          menu{' '}
+        </li>
+        <li
+          onClick={() => setMenu('about')}
+          className={menu === 'about' ? 'underline' : ''}
+        >
+          about{' '}
+        </li>
+        <li
+          onClick={() => setMenu('contact')}
+          className={menu === 'contact' ? 'underline' : ''}
+        >
+          contacts{' '}
+        </li>
+      </ul>
       <div className='flex gap-2'>
         <input type='search' className='' />
         <button className='bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full flex gap-2 place-items-center'>
