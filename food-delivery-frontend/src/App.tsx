bb31c97095e5d9033eaf4903d9a24aac87f09462
kennzuki <kingsyken@gmail.com>
2025-11-04 15:04:17 +0300

    initial commit

---
+++ food-delivery-frontend/src/App.tsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+ 
+
+  return (
+    
+       <div>ken</div>
+  )
+}
+
+export default App
---
+++ food-delivery-frontend/src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <div className=''>
+      <h1 className='text-3xl font-bold underline uppercase'> Kenki deli </h1>
+    </div>
+  );
+};
+
+export default Navbar;
